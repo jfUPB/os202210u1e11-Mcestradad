dc29d9aef354b9ec57f3c9feb5491f184f900bb2
Mcestradad <72944555+Mcestradad@users.noreply.github.com>
2022-02-12 16:45:02 -0500

    "V5"

--- problem1/eventList.c
+++ problem1/eventList.c
@@ -5,22 +5,21 @@
 
 EventList *CreateEventList(void)
 {
-    EventList *eventList = NULL;
+    EventList *eventList = malloc(sizeof(eventList));
 
     return eventList;
 }
 
 void DestroyEventList(EventList *this)
 {
-
-
     free(this);
 }
 
 Event *SearchEvent(EventList *this, char *name)
 {
-    Event *a = this->head; //Evento de referencia para comenzar a buscarlo en la lista.
-    if (this->isEmpty == 0) //Revisar si la lista está vacía.
+
+    Event *a = this->head;
+    if (this->isEmpty == 0)
     {
         return NULL;
     }
@@ -28,90 +27,52 @@ Event *SearchEvent(EventList *this, char *name)
     else
     {
        
-        while (a != NULL)  //Cuando no está vacía.
+        while (a != NULL)
         {
-
-            int longitud_arr1= sizeof(a->eventName);
-            int longitud_arr2= sizeof(*name);
-
-            if (longitud_arr1 != longitud_arr2)
-            {
-                //printf("No existe el evento");
-                return NULL;
-            }
-            for (int i = 0; i < longitud_arr1; i++) {  //Puede ser cualquier longitud ya que son iguales
-                // Obtener elementos de ambos arreglos en misma posición o índice
-                char valor_arr1 = a->eventName[i], valor_arr2 = name[i];
-
-                // Comparar 
-                if (valor_arr1 == valor_arr2) 
-                {
-                    
-                    return a; 
-                }
-            }
-
             
-
+            if (*(a->eventName + 2) == *(name + 2) && *(a->eventName + 3) == *(name + 3))
+            {
+                return a;
+            }
+            
             a = a->next;
         }
     }
-    
+
     DestroyEvent(a);
-    return NULL;
-}
 
     return NULL;
 }
 
 void AddEvent(EventList *this, Event *event)
 {
-    Event *a = this->head; //Evento de referencia para comenzar a buscarlo en la lista.
-    if (this->isEmpty == 0) //Revisar si la lista está vacía.
-    {
-        return NULL;
-    }
-    
-    else
-    {
-       
-        while (a != NULL)  //Cuando no está vacía.
+    Event *a = this->head;
+
+    if (this->isEmpty!=0){
+        while (a != NULL)
         {
-
-            int longitud_arr1= sizeof(a->eventName);
-            int longitud_arr2= sizeof(*name);
-
-            if (longitud_arr1 != longitud_arr2)
-            {
-                //printf("No existe el evento");
-                return NULL;
-            }
-            for (int i = 0; i < longitud_arr1; i++) {  //Puede ser cualquier longitud ya que son iguales
-                // Obtener elementos de ambos arreglos en misma posición o índice
-                char valor_arr1 = a->eventName[i], valor_arr2 = name[i];
-
-                // Comparar 
-                if (valor_arr1 == valor_arr2) 
-                {
-                    
-                    return a; 
-                }
-            }
-
             
-
+            if(*(a->eventName+2)==*(event->eventName+2) && *(a->eventName+3)==*(event->eventName+3)){
+                return;
+            }
+            
             a = a->next;
         }
+
+        this->last->next=event;
+        this->last=event;
     }
-    
+    else{
+        this->head = event;
+        this->last = event;
+        this->isEmpty= 1;
+    }
     DestroyEvent(a);
-    return NULL;
 }
-
-
 
 void RemoveEvent(EventList *this, char *name)
 {
+    
     Event *a = this->head;
 
     Event *t = SearchEvent(this, name);
@@ -129,19 +90,18 @@ void RemoveEvent(EventList *this, char *name)
         while (a != NULL)
         {
             
-            
-            if (*(this->head->eventName + 2) == *(name + 2))
+            if (*(this->head->eventName + 2) == *(name + 2) && *(this->head->eventName + 3) == *(name + 3))
             {
                 this->head = this->head->next;
                 break;
             }
 
-            else if (*(a->next->eventName + 2) == *(name + 2))
+            else if (*(a->next->eventName + 2) == *(name + 2) && *(a->next->eventName + 3) == *(name + 3))
             {
                 a->next = a->next->next;
                 break;
             }
-
+            
             a = a->next;
             
         }
@@ -152,18 +112,20 @@ void RemoveEvent(EventList *this, char *name)
     }
 }
 
+
 void ListEvents(EventList *this)
 {
     Event *a = this->head;
     
     if (this->isEmpty == 0)
         printf("empty\n");
+        
     else
     {
         while (a != NULL)
         {
-            printf("%s\n", a->eventName); 
             a = a->next;
         }
     }
+    DestroyEvent(a);
 }
